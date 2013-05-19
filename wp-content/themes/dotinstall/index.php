ffbea6f4a67f47e1b88ded71a0e2f34c815c4110
Yuta Matsumura <dev.tsubaki@gmail.com>
2013-05-19 15:50:32 +0900

    Create dotinstall theme

---
+++ wp-content/themes/dotinstall/index.php
@@ -0,0 +1,58 @@
+﻿<!doctype html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>はじめてのWordPress</title>
+	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.9.0/build/reset/reset-min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="header" class="container">
+		<h1>
+			<a href="">はじめてのWordPress</a>
+			<ul class="menu">
+				<li><a href="">menu</a></li>
+				<li><a href="">menu</a></li>
+				<li><a href="">menu</a></li>
+			</ul>
+		</h1>
+	</div><!-- /header -->
+	<div id="main" class="container">
+		<div id="posts">
+			<div id="post">
+				<div class="post-header">
+					<h2><a href="">タイトル</a></h2>
+					<div class="post-meta">
+						2013年5月19日 【カテゴリ】
+					</div>
+				</div>
+				<div class="post-content">
+					<div class="post-image">
+						<img src="img/noimage.png" width="100" height="100">
+					</div>
+					<div class="post-body">
+						<p>本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。</p>
+					</div>
+				</div>
+			</div><!-- /post -->
+			<div class="navigation">
+				<div class="prev">prev</div>
+				<div class="next">next</div>
+			</div>
+		</div><!-- /posts -->
+		<div id="sidebar">
+			<div class="widget">
+				<h3>カテゴリ</h3>
+				<ul>
+					<li><a href="">item</a></li>
+					<li><a href="">item</a></li>
+					<li><a href="">item</a></li>
+				</ul>
+			</div>
+		</div><!-- /sidebar -->
+	</div><!-- /main -->
+	<div id="footer" class="container">
+		footer
+	</div><!-- /footer -->
+</body>
+</html>
