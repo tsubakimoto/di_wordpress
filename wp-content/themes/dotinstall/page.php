939720707f8b8831d2b67df76446feaf745f0072
Yuta Matsumura <dev.tsubaki@gmail.com>
2013-05-19 21:56:14 +0900

    Added page.php and edited index.php, single.php

--- wp-content/themes/dotinstall/page.php
+++ wp-content/themes/dotinstall/page.php
@@ -10,9 +10,6 @@
 			<div id="post">
 				<div class="post-header">
 					<h2><a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a></h2>
-					<div class="post-meta">
-						<?php echo get_the_date(); ?> 【<?php the_category(', '); ?>】
-					</div>
 				</div>
 				
 				<div class="post-content">
@@ -20,17 +17,12 @@
 				</div>
 			</div><!-- /post -->
 			
-			<div class="navigation">
-				<div class="prev"><?php previous_post_link(); ?></div>
-				<div class="next"><?php next_post_link(); ?></div>
-			</div>
-			
 			<?php
 				endwhile;
 			else:
 			?>
 			
-			<p>記事はありません。</p>
+			<p>ページはありません。</p>
 			
 			<?php
 			endif;
